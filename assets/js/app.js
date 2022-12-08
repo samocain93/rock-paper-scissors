2ea7ead875537bcfb77cde3bddb0fa41564c8e09
Sam O'Cain <samocain@Sams-MBP.lan>
2022-12-08 00:00:45 -0600

    set up const variables

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// TODO: Create elements for page and begin functionality
+
+const userScoreEl = document.getElementById("user-score");
+const computerScoreEl = document.getElementById("computer-score");
+const winnerMessageEl = document.getElementById("winner");
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const buttonEl = document.getElementById("restart-btn");
